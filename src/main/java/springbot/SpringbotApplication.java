e8b414c5330bc195788c6cc51f3b70b968e76bcf
rainett <86346775+valera3450zhma@users.noreply.github.com>
2022-08-23 19:05:01 +0300

    empty project

---
+++ src/main/java/springbot/SpringbotApplication.java
@@ -0,0 +1,13 @@
+package springbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbotApplication.class, args);
+	}
+
+}
